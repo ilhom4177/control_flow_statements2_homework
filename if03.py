19773e7239e3af2559a6be4fce36b9aed6626236
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-23 09:17:23 +0500

    if03.py

--- if03.py
+++ if03.py
@@ -8,4 +8,15 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
-    return
+    if a!=b:
+        if a!=c:
+            return(a)
+        else:
+            return(c)
+
+    else:
+        if b!=c:
+            return(b)
+        else:
+            return(c) 
+print(main(5,5,1))
