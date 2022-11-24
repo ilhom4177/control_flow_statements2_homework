6c7ea251826dcec71fa9e31700ef93519d7b8854
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-24 18:08:35 +0500

    if05.py

--- if05.py
+++ if05.py
@@ -6,6 +6,30 @@ def main(n):
     Returns:
         int: return answer.
     """
-   git
+    n1=n%10
+    n//=10
+
+    n2=n%10
+    n//=10
+
+    n3=n%10
+    n//=10
+
+    n4=n%10
+    n//=10
+
+    n5=n%10
+
+    if n1>n5:
+        n = n1
+    if n2>n:
+        n=n2
+    if n3>n:
+        n=n3
+    if n4>n:
+        n=n4
+    if n5 >n:
+        n=n5
+    return n
 
 print(main(23546))
