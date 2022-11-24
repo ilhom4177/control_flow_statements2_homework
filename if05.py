39655b41d8a1114111f5337b4b447a199c7af975
Ilhom <ilhommamatqulov276@gmail.com>
2022-11-24 18:00:57 +0500

    if05.py

--- if05.py
+++ if05.py
@@ -6,33 +6,6 @@ def main(n):
     Returns:
         int: return answer.
     """
-    a=n%10
-    n//=10
-
-    b=n%10
-    n//=10
-
-    c=n%10
-    n//=10
-
-    d=n%10
-    n//=10
-
-    e=n%10
-
-    if a>b and a>c and a>d and a>e:
-        return a
-
-    elif b>a and b>c and b>d and b>e:
-        return b
-
-    elif c>a and c>b and c>d and c>e:
-        return c
-
-    elif d>a and d>b and d>c and d>e:
-        return d
-
-    elif e>a and e>b and e>c and e>d:
-        return e
+   git
 
 print(main(23546))
